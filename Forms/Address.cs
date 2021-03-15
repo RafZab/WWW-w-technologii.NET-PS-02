5c0fda2ece59db2d4b7f356ec3e0d6697c3d4eb2
Rafał Zaborowski <raffik2000@gmail.com>
2021-03-15 16:19:18 +0100

    First commit

---
+++ Forms/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdressBook.Forms
+{
+    public class Address
+    {   
+        [Display(Name = "Your favorite street")]
+        public string Street { get; set; }
+
+        [Display(Name = "Zip code")]
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using AdressBook.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdressBook.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public Address Address { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+    }
+}
